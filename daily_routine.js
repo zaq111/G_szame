ef0051ed61d9e412b9b7f808cc87983295667610
zaq111 <sin1983@yahoo.com.sg>
2026-05-26 15:50:33 +0700

    Update daily_routine.js

--- daily_routine.js
+++ daily_routine.js
@@ -13,28 +13,23 @@ async function runDailyRoutine(bagian) {
     if (bagian === 1 || bagian === undefined) {
         logBagian(1, "Raid Boss & Dungeon Daily");
         kirimPerintah("Raid Boss", [255,221,6,0,108,189,67,208,86,0,0,0,53,83]);
-        await tunggu(500);
+        await tunggu(100);
         kirimPerintah("Challenge - Dungeon Daily", [255,221,7,0,103,58,38,138,32,8,0,0,53,68,1]);
-        await tunggu(500);
+        await tunggu(100);
         kirimPerintah("Raid - Dungeon Daily", [255,221,7,0,248,229,216,19,54,8,0,0,53,68,2]);
-        await tunggu(500);
+        await tunggu(100);
     }
 
     // JIKA MEMILIH BAGIAN 2 (atau jalankan semua)
     if (bagian === 2 || bagian === undefined) {
         logBagian(2, "Abyss Operations");
         kirimPerintah("Abyss - Reset Confirm", [255,221,6,0,143,71,126,108,186,27,0,0,53,32]);
-        await tunggu(500);
 		kirimPerintah("Abyss - Quick Raid", [255,221,10,0,169,182,45,131,120,8,0,0,0,32,44,0,0,0]);
-        await tunggu(500);
-		kirimPerintah("Abyss - Quick Raid Confirm", [255,221,6,0,236,101,82,170,141,8,0,0,53,33]);
-        await tunggu(500);
+        kirimPerintah("Abyss - Quick Raid Confirm", [255,221,6,0,236,101,82,170,141,8,0,0,53,33]);
 		kirimPerintah("Claim Gift Teritory", [255,221,6,0,70,187,104,162,197,0,0,0,11,6]);
-        await tunggu(500);
 		kirimPerintah("Chat Cross Server", [255,221,15,0,57,165,90,172,37,12,0,0,59,1,10,0,0,0,0,1,0,97,0]);
-        await tunggu(500);
 		kirimPerintah("Buy Dark Spirit Ethereal Realm", [255,221,6,0,184,175,234,159,229,1,0,0,68,35],50)
-        await tunggu(500);
+        await tunggu(100);
 		kirimPerintah("Claim Dark Spirit Ethereal Realm 1", [255,221,7,0,19,57,164,237,228,8,0,0,68,36,1]);
 		kirimPerintah("Claim Dark Spirit Ethereal Realm 2", [255,221,7,0,19,57,164,237,228,8,0,0,68,36,2]);
 		kirimPerintah("Claim Dark Spirit Ethereal Realm 3", [255,221,7,0,19,57,164,237,228,8,0,0,68,36,3]);
@@ -44,14 +39,17 @@ async function runDailyRoutine(bagian) {
         await tunggu(500);
 		kirimPerintah("Diaz Donate Guild", [255,221,10,0,47,173,9,230,229,29,0,0,62,13,1,0,0,0],500);
 		kirimPerintah("Gold Donate Guild", [255,221,10,0,47,173,9,230,229,29,0,0,62,13,2,0,0,0],100);
-        await tunggu(20000);
+		console.log("tunggu 25 detik");
+        await tunggu(25000);
 		kirimPerintah("Enhance Gear", [255,221,7,0,87,17,135,83,71,0,0,0,52,1,0]);
 		kirimPerintah("Upgrade Skill", [255,221,8,0,245,242,128,19,5,1,0,0,2,4,7,0]);
 		kirimPerintah("Train Avatar", [255,221,6,0,124,222,183,88,89,2,0,0,57,32]);
 		kirimPerintah("Train Mercenary", [255,221,6,0,216,240,70,155,178,13,0,0,57,12]);
 		kirimPerintah("AFK Raid", [255,221,7,0,126,24,103,6,167,7,0,0,53,7,2]);
+		await tunggu(2500);
 		kirimPerintah("Inv Guild Team Challenge", [255,221,6,0,69,113,27,184,5,16,0,0,67,26]);
 		kirimPerintah("Start Guild Team Challenge", [255,221,6,0,252,31,178,53,71,16,0,0,67,13]);
+		console.log("tunggu 8 detik");
 		await tunggu(8000);
 		kirimPerintah("Open Rune Essence", [255,221,15,0,164,42,62,163,238,28,0,0,3,6,11,226,4,0,1,0,0,0,1],100);
 		kirimPerintah("Open Rune Essence", [255,221,15,0,127,90,139,27,173,29,0,0,3,6,10,226,4,0,1,0,0,0,1],100);
@@ -61,6 +59,7 @@ async function runDailyRoutine(bagian) {
 		kirimPerintah("Open Box Territory War", [255,221,15,0,101,4,124,151,85,31,0,0,3,6,239,89,5,0,1,0,0,0,1],20)
 		kirimPerintah("Open Box Premium Circle", [255,221,15,0,169,229,236,104,228,31,0,0,3,6,65,187,4,0,1,0,0,0,1],20)
 		kirimPerintah("Up Dark Spirit", [255,221,6,0,79,239,158,140,127,12,0,0,68,31],2000)
+		console.log("tunggu 10 detik");
 		await tunggu(10000);
 		kirimPerintah("Buy Sprite Organite 1", [255,221,13,0,100,151,204,7,86,1,0,0,61,28,1,1,0,1,0,0,0]);
 		kirimPerintah("Buy Sprite Organite 2", [255,221,13,0,100,151,204,7,86,1,0,0,61,28,1,2,0,1,0,0,0]);
@@ -77,7 +76,7 @@ async function runDailyRoutine(bagian) {
 		kirimPerintah("Buy Sprite Organite 13", [255,221,13,0,100,151,204,7,86,1,0,0,61,28,1,13,0,1,0,0,0]);
 		kirimPerintah("Buy Sprite Organite 14", [255,221,13,0,100,151,204,7,86,1,0,0,61,28,1,14,0,1,0,0,0]);
 		kirimPerintah("Buy Sprite Organite 15", [255,221,13,0,100,151,204,7,86,1,0,0,61,28,1,15,0,1,0,0,0]);
-		await tunggu(3000);
+		await tunggu(1000);
 		kirimPerintah("Claim Benefit Hall 2min", [255,221,8,0,194,215,83,64,89,12,0,0,36,31,1,0]);
 		kirimPerintah("Claim Benefit Hall 5min", [255,221,8,0,59,220,189,231,124,12,0,0,36,31,2,0]);
 		kirimPerintah("Claim Benefit Hall 10min", [255,221,8,0,59,220,189,231,124,12,0,0,36,31,3,0]);
@@ -90,14 +89,14 @@ async function runDailyRoutine(bagian) {
 		kirimPerintah("Claim Benefit Hall 120min", [255,221,8,0,59,220,189,231,124,12,0,0,36,31,10,0]);
 		kirimPerintah("Claim Benefit Hall 150min", [255,221,8,0,59,220,189,231,124,12,0,0,36,31,11,0]);
 		kirimPerintah("Claim Benefit Hall 180min", [255,221,8,0,59,220,189,231,124,12,0,0,36,31,12,0]);
-		await tunggu(2000);
+		await tunggu(1000);
 		kirimPerintah("Claim Login Gift", [255,221,7,0,99,61,207,116,19,20,0,0,36,41,1]);
 		kirimPerintah("Claim Login Gift", [255,221,7,0,99,61,207,116,19,20,0,0,36,41,2]);
 		kirimPerintah("Claim Login Gift", [255,221,7,0,99,61,207,116,19,20,0,0,36,41,3]);
 		kirimPerintah("Claim Login Gift", [255,221,7,0,99,61,207,116,19,20,0,0,36,41,4]);
 		kirimPerintah("Claim Login Gift", [255,221,7,0,99,61,207,116,19,20,0,0,36,41,5]);
 		kirimPerintah("Claim Login Gift", [255,221,7,0,99,61,207,116,19,20,0,0,36,41,6]);
-		await tunggu(2000);
+		await tunggu(1000);
 		kirimPerintah("Gear Hunt", [255,221,9,0,193,72,160,192,210,20,0,0,6,2,1,1,0],2);
 		kirimPerintah("Soul Gear Hunt", [255,221,9,0,51,40,176,139,204,21,0,0,6,2,2,1,0],2);
 		kirimPerintah("Rune Hunt", [255,221,9,0,193,72,160,192,210,20,0,0,6,2,3,1,0],2);
@@ -151,27 +150,27 @@ async function runDailyRoutine(bagian) {
             await tunggu(1000); // Jeda sebentar setelah beli
 
             kirimPerintah("Loch Temple M11 R4", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,19,0]);
-            await tunggu(80000);
+            await tunggu(70000);
             kirimPerintah("Loch Temple M11 R2", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,18,0]);
-            await tunggu(80000);
+            await tunggu(70000);
             kirimPerintah("Loch Temple M10 R4", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,17,0]);
-            await tunggu(80000);
+            await tunggu(70000);
             kirimPerintah("Loch Temple M10 R2", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,16,0]);
-            await tunggu(80000);
+            await tunggu(70000);
             kirimPerintah("Loch Temple M9 R4", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,15,0]);
-            await tunggu(80000);
+            await tunggu(70000);
 
             kirimPerintah("Loch Temple M9 R2", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,14,0]);
-            await tunggu(80000);
+            await tunggu(70000);
 
             kirimPerintah("Loch Temple M8 R4", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,13,0]);
-            await tunggu(80000);
+            await tunggu(70000);
 
             kirimPerintah("Loch Temple M8 R2", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,12,0]);
-            await tunggu(80000);
+            await tunggu(70000);
 
             kirimPerintah("Loch Temple M7 R4", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,11,0]);
-            await tunggu(80000);
+            await tunggu(70000);
 
             kirimPerintah("Loch Temple M7 R2", [255,221,8,0,126,56,17,215,189,0,0,0,53,194,10,0]);
             console.log("[Bot] Semua urutan perintah Loch Temple selesai! Bersiap transisi...");
